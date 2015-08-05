08a8bd2d628292252f395f74249b26f63c0d7afb
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2015-08-05 18:37:54 +0500

    Fixed course_id pattern regex. PLAT-776

--- common/djangoapps/track/tests/test_contexts.py
+++ common/djangoapps/track/tests/test_contexts.py
@@ -1,23 +1,35 @@
 # pylint: disable=missing-docstring
 
+import ddt
 from unittest import TestCase
 
 from track import contexts
 
 
+@ddt.ddt
 class TestContexts(TestCase):
 
     COURSE_ID = 'test/course_name/course_run'
+    SPLIT_COURSE_ID = 'course-v1:test+course_name+course_run'
     ORG_ID = 'test'
 
-    def test_course_id_from_url(self):
-        self.assert_parses_course_id_from_url('http://foo.bar.com/courses/{course_id}/more/stuff')
+    @ddt.data(
+        ('http://foo.bar.com/courses/{course_id}', COURSE_ID),
+        ('http://foo.bar.com/courses/{course_id}/more/stuff', COURSE_ID),
+        ('http://foo.bar.com/courses/{course_id}?format=json', COURSE_ID),
+        ('http://foo.bar.com/courses/{course_id}', SPLIT_COURSE_ID),
+        ('http://foo.bar.com/courses/{course_id}/more/stuff', SPLIT_COURSE_ID),
+        ('http://foo.bar.com/courses/{course_id}?format=json', SPLIT_COURSE_ID)
+    )
+    @ddt.unpack
+    def test_course_id_from_url(self, url, course_id):
+        self.assert_parses_course_id_from_url(url, course_id)
 
-    def assert_parses_course_id_from_url(self, format_string):
+    def assert_parses_course_id_from_url(self, format_string, course_id):
         self.assertEquals(
-            contexts.course_context_from_url(format_string.format(course_id=self.COURSE_ID)),
+            contexts.course_context_from_url(format_string.format(course_id=course_id)),
             {
-                'course_id': self.COURSE_ID,
+                'course_id': course_id,
                 'org_id': self.ORG_ID
             }
         )
@@ -37,8 +49,17 @@ class TestContexts(TestCase):
     def test_malformed_course_id(self):
         self.assert_empty_context_for_url('http://foo.bar.com/courses/test')
 
-    def test_course_id_later_in_url(self):
-        self.assert_parses_course_id_from_url('http://foo.bar.com/x/y/z/courses/{course_id}')
+    @ddt.data(
+        ('http://foo.bar.com/x/y/z/courses/{course_id}', COURSE_ID),
+        ('http://foo.bar.com/x/y/z/courses/{course_id}/more/stuff', COURSE_ID),
+        ('http://foo.bar.com/x/y/z/courses/{course_id}?format=json', COURSE_ID),
+        ('http://foo.bar.com/x/y/z/courses/{course_id}', SPLIT_COURSE_ID),
+        ('http://foo.bar.com/x/y/z/courses/{course_id}/more/stuff', SPLIT_COURSE_ID),
+        ('http://foo.bar.com/x/y/z/courses/{course_id}?format=json', SPLIT_COURSE_ID)
+    )
+    @ddt.unpack
+    def test_course_id_later_in_url(self, url, course_id):
+        self.assert_parses_course_id_from_url(url, course_id)
 
     def test_no_url(self):
         self.assert_empty_context_for_url(None)
